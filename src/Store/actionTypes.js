59bc3ab7c84261d7f775a6c264983225478d6fc1
Shohab Ahmed <shohabahmedk@gmail.com>
2021-03-15 09:50:43 +0500

    add

---
+++ src/Store/actionTypes.js
@@ -0,0 +1,16 @@
+// Auth
+export const LOGIN = "LOGIN_CLIENT";
+export const AUTHENTICATE = "AUTHENTICATE";
+export const LOGOUT = "LOGOUT";
+export const SIGNUP = "USER_SIGNUP";
+export const GET_VEHICLES = "GET_VEHICLES";
+export const GET_PROFILE = "GET_PROFILE";
+
+// Location
+export const GET_LOCATION = "GET_LOCATION";
+export const CHANGE_PASSWORD = "CHANGE_PASSWORD";
+export const VERIFICATION_CODE = "VERIFICATION_CODE";
+export const SET_TRAVEL_LOCATION = "SET_TRAVEL_LOCATION";
+
+// Documents
+export const GET_ALL_DOCUMENT_TYPES = "GET_ALL_DOCUMENT_TYPES";
---
+++ src/Store/server.js
@@ -0,0 +1,1 @@
+export const Api = "https://epick.code4client.com";
